7fc10039dc1c10739bc2f4749061f15c0444a256
KataRiegels <62016188+KataRiegels@users.noreply.github.com>
2023-05-05 15:58:28 +0200

    key problems

--- src/keyManager.js
+++ src/keyManager.js
@@ -16,15 +16,15 @@ AESKey={}
 
 
   encryptAESKeyWithPublicKey(publicKeyOfRecipient){
-      window.crypto.subtle.encrypt(
+      let encryptedKey=window.crypto.subtle.encrypt(
         {
             name: "RSA-OAEP",
             //label: Uint8Array([...]) //optional
         },
-        publicKey, //from generateKey or importKey above
-        data //ArrayBuffer of data you want to encrypt
+        publicKeyOfRecipient, //from generateKey or importKey above
+        this.AESKey //ArrayBuffer of data you want to encrypt
     )
-    .then(function(encryptedKey){
+    .then(function(){
         //returns an ArrayBuffer containing the encrypted data
         return(new Uint8Array(encryptedKey));
         
@@ -39,13 +39,13 @@ AESKey={}
         async GenerateAESKey() {
         let AESKeyObject=await window.crypto.subtle.generateKey(
           {
-              name: "AES-CTR",
+              name: "AES-GCM",
               length: 256, //can be  128, 192, or 256
           },
           true, //whether the key is extractable (i.e. can be used in exportKey)
           ["encrypt", "decrypt"] //can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
       )
-        let AESKeyExportedToJson=await crypto.subtle.exportKey("jwk", AESKeyObject);
+        let AESKeyExportedToJson=await crypto.subtle.exportKey("spki", AESKeyObject);
         this.AESKey=AESKeyExportedToJson
         return AESKeyExportedToJson
 
@@ -54,17 +54,17 @@ AESKey={}
       async generateRSAKeyPair(){
        let keyPair = await  window.crypto.subtle.generateKey(
           {
-              name: "RSASSA-PKCS1-v1_5",
+              name: "RSA-OAEP",
               modulusLength: 2048, //can be 1024, 2048, or 4096
               publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
               hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
           },
           true, //whether the key is extractable (i.e. can be used in exportKey)
-          ["sign", "verify"] //can be any combination of "sign" and "verify"
+          ["encrypt", "decrypt"] //can be any combination of "sign" and "verify"
       )
-      let publicKey=  await crypto.subtle.exportKey("jwk", keyPair.publicKey);
+      let publicKey=  await crypto.subtle.exportKey("spki", keyPair.publicKey);
       //let publicKeyString =  JSON.stringify(publicKey, null, " ");
-       let privateKey=  await crypto.subtle.exportKey("jwk", keyPair.privateKey);
+       let privateKey=  await crypto.subtle.exportKey("spki", keyPair.privateKey);
        this.privateKey=privateKey;
        this.publicKey=publicKey;
        return   [publicKey,privateKey]
